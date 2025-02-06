a2c5232c13f38562c3208a51c8001cb0e55c8858
Klez <118447126+klez69@users.noreply.github.com>
2025-02-06 11:34:27 +0100

    Create dbt.php

---
+++ dbt.php
@@ -0,0 +1,5 @@
+// Dane do połączenia z bazą danych
+$host = 'localhost'; // Adres hosta
+$dbname = 'nazwa_bazy'; // Nazwa bazy danych
+$user = 'uzytkownik'; // Nazwa użytkownika bazy danych
+$password = 'haslo'; // Hasło użytkownika bazy danych
