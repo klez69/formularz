1ab6e68bc8d0a3d68623ec4047133b68edcc802d
Klez <118447126+klez69@users.noreply.github.com>
2025-02-11 13:22:05 +0100

    Update get_client.php

--- get_client.php
+++ get_client.php
@@ -14,12 +14,13 @@ if (isset($_GET['wojewodztwo'])) {
     while ($row = fgetcsv($handle)) {
         if ($row[0] === $wojewodztwo) {
             echo json_encode([
-                'imie_nick' => $row[1],
-                'adres' => $row[2],
-                'kod' => $row[3],
-                'miasto' => $row[4],
-                'telefon' => $row[5],
-                'email' => $row[6]
+                'firma' => $row[1],
+                'imie_nick' => $row[2],
+                'adres' => $row[3],
+                'kod' => $row[4],
+                'miasto' => $row[5],
+                'telefon' => $row[6],
+                'email' => $row[7]
             ]);
             fclose($handle);
             exit;
