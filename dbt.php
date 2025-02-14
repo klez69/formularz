e903cf8de364f210aedf23328a0a0b706d3cf7e4
Klez <118447126+klez69@users.noreply.github.com>
2025-02-14 10:01:55 +0100

    Update dbt.php

--- dbt.php
+++ dbt.php
@@ -1,5 +1,7 @@
+<?
 // Dane do połączenia z bazą danych
 $host = 'mysql.ibc.pl'; // Adres hosta
 $dbname = 'baza5950_brcmaestro'; // Nazwa bazy danych
 $user = 'admin5950_brcmaestro'; // Nazwa użytkownika bazy danych
 $password = 'h5C)f0Z)e3'; // Hasło użytkownika bazy danych
+?>
