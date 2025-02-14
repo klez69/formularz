5d4a19e1ea038e4e52141eec9c043ea701a83998
Klez <118447126+klez69@users.noreply.github.com>
2025-02-14 12:21:31 +0100

    Update dbt.php

--- dbt.php
+++ dbt.php
@@ -5,3 +5,4 @@ $username   = "admin5950_brcmaestro";         // dostosuj do swoich ustawień
 $password   = "h5C)f0Z)e3";             // dostosuj do swoich ustawień
 $dbname     = "baza5950_brcmaestro";     // podaj nazwę swojej bazy danych
 ?>
+
